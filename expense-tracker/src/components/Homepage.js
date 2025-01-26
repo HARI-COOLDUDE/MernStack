6f132c4c2f2a4170beeafc2edfc4028f1979a49f
HARI-COOLDUDE <harimilan450@gmail.com>
2025-01-26 14:26:11 +0530

    Second Commit

---
+++ expense-tracker/src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import img1 from "../asset/img/dashboard.jpeg"
+function Homepage() {
+  return (
+    <div className="container">
+        <div className="leftcontainer">
+            <p>hi</p>
+            <img src={img1}/>
+        </div>
+        <div className="rightcontainer">
+            <p>hlo</p>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
+
